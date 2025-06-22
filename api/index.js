5a0f82dd5b432a02c754b1f145db39a8e303fad3
prashant_sonawane <p2352002@gmail.com>
2025-06-22 17:28:32 +0530

    database connection setup

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("comefoef ");
+}).catch((err)=>{
+    console.log(err);
+})
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log("hello");
-})
+app.listen(3000, () => {
+  console.log("hello");
+});
