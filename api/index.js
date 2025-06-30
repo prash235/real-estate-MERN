b4ea75f24fd478dc41952e0768dc9275fb930b11
prashant_sonawane <p2352002@gmail.com>
2025-06-30 23:08:42 +0530

    signup api added

--- api/index.js
+++ api/index.js
@@ -2,17 +2,21 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("comefoef ");
+    console.log("connected to the MongoDB ");
 }).catch((err)=>{
     console.log(err);
 })
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("hello");
 });
 
 app.use('/api/user',userRouter)
+app.use('/api/auth',authRouter)
